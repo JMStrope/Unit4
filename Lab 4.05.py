39cb6a7f09665de3edfce05dfb7ed8af61afc427
Jack <Jmstrope24@students.bmchs.org>
2022-04-25 09:39:08 -0500

    ADDED

--- Lab 4.05.py
+++ Lab 4.05.py
@@ -30,3 +30,29 @@ Write down any bugs that you see in the program
 Fix the 2 programs above.
 Create a program that asks the user which function they would like to run.
 '''
+num_list = [1, 2, 3, 4, 5, 6]
+def print_numbers(list):
+    for i in range(len(list)):
+        print(list[i])
+
+
+
+def swapping_stars():
+    line_str = ""
+    for line in range(0, 5):
+        for char in range(0,5):
+            if line % 2 == char % 2:
+                    line_str += "*"
+            else:
+                    line_str += "-"
+    print(line_str)
+
+
+while True:
+    user_input = input("Which function would you like to run? 1 or 2 > ")
+    if user_input == "1":
+        print_numbers(num_list)
+    elif user_input == "2":
+        swapping_stars()
+    else:
+        print("Invalid Command")
